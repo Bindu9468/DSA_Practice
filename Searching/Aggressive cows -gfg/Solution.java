5e43be5d6af2656e9d164e1221e725ab6c5b2218
Bindu9468 <bindupriyar430@gmail.com>
2025-05-07 18:11:36 +0530

    aggressive cows

--- Searching/Aggressive cows -gfg/Solution.java
+++ Searching/Aggressive cows -gfg/Solution.java
@@ -1,32 +1,7 @@
-//{ Driver Code Starts
-// Initial Template for Java
 
-import java.io.*;
-import java.util.*;
+// User function Template for Java
 
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-
-        while (tc-- > 0) {
-
-            String[] str = br.readLine().trim().split(" ");
-            int[] a = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                a[i] = Integer.parseInt(str[i]);
-            }
-            String[] nk = br.readLine().trim().split(" ");
-            int k = Integer.parseInt(nk[0]);
-            Solution sln = new Solution();
-            int ans = sln.aggressiveCows(a, k);
-
-            System.out.println(ans);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
+import java.util.Arrays;
 
 // User function Template for Java
 class Solution {
@@ -66,6 +41,22 @@ class Solution {
 
         }
         return false;
+    }
+
+    public static void main(String[] args) {
+        int[] stalls1 = { 1, 2, 4, 8, 9 };
+        int k1 = 3;
+        System.out.println(aggressiveCows(stalls1, k1)); // Output: 3
+
+        /*
+         * int[] stalls2 = { 10, 1, 2, 7, 5 };
+         * int k2 = 3;
+         * System.out.println(aggressiveCows(stalls2, k2)); // Output: 4
+         * 
+         * int[] stalls3 = { 2, 12, 11, 3, 26, 7 };
+         * int k3 = 5;
+         * System.out.println(aggressiveCows(stalls3, k3)); // Output: 1
+         */
     }
 }
 
